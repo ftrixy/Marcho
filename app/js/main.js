17de4acbd4117c016f962724f1dc44e7b9ae1033
ftrixy <dmitry.kravetss@gmail.com>
2021-12-12 17:14:55 +0200

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function() {
         fade: true,
         autoplay: true,
     });
+
+    $('.star').rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+    });
 });
